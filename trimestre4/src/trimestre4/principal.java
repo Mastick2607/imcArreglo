6eba1a3d350280a742732fddb5da19db166ef61e
marlon <mstick2607@gmail.com>
2023-05-25 10:53:42 -0500

    imc_arreglo

---
+++ trimestre4/src/trimestre4/principal.java
@@ -0,0 +1,65 @@
+package trimestre4;
+
+public class principal {
+	
+	public static void main(String[] arg) {
+		
+	
+		
+//	sumaMetodo sm = new sumaMetodo();
+		
+	//	sm.iniciar();
+		
+	//	numNoRepes nnp = new numNoRepes ();
+		
+		//nnp.Iniciar();
+		
+	/*	validarNum  vn = new validarNum();
+		
+		vn.iniciar();*/
+		
+	/*
+	 * 	sumaArreglosMetodos  sam = new sumaArreglosMetodos();
+	 * sam.iniciar();
+	 */
+		
+      /*
+       *  ejemploArreglo ea = new ejemploArreglo(); 
+       *  
+       *  ea.ejemArreglo();
+		
+       */
+
+		 /*  estudiantes e = new estudiantes();
+      e.iniciar();*/
+		
+	/*viajePremio v = new  viajePremio();
+		
+	   v.iniciar();*/
+		
+	//	parImparHashMap pi = new parImparHashMap();
+		/*
+		 * ciudadesXPaises  cxp = new  ciudadesXPaises();
+		 *  cxp.iniciar();
+		 */
+		
+		
+		
+		 /*	 procesos p = new procesos();
+		 p.iniciar();*/
+		
+		
+	/*cursoEstudiante cs= new cursoEstudiante();
+	
+	cs.iniciar();
+	} */
+		
+	/*	imcArrayList imc = new imcArrayList();
+	
+       imc.menu();*/
+	
+		imcArreglo imca= new imcArreglo();
+		imca.menu();
+	
+	}
+	}
